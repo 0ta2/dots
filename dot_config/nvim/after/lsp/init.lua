9fd104c206c07b7890dfce76895a1afe34f83258
0ta2 <mangamania5643+github@gmail.com>
2025-07-18 23:50:37 +0900

    feat(nvim): lspの設定をnvim 0.11以降の記法に変更

---
+++ dot_config/nvim/after/lsp/init.lua
@@ -0,0 +1,3 @@
+return vim.lsp.config("*", {
+    capabilities = require('blink.cmp').get_lsp_capabilities(),
+})
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -12,16 +12,6 @@ return {
             { "mason-org/mason.nvim", opts = {} },
             {
                 "neovim/nvim-lspconfig",
-                opts = {
-                    servers = lsp_server
-                },
-                config = function(_, opts)
-                    local lspconfig = require('lspconfig')
-                    for server, config in pairs(opts.servers) do
-                        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-                        lspconfig[server].setup(config)
-                    end
-                end
             },
         },
     },
