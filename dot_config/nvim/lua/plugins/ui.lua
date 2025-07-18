02b1cc0d6ae72547ffc16553e6d70ce7ca9c678b
0ta2 <mangamania5643+github@gmail.com>
2025-07-19 01:58:53 +0900

    feat(nvim): snacks.nvimのdashboardを有効化してカスタマイズの実施

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,7 +1,51 @@
 return {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-        require('lspsaga').setup({})
-    end,
-    opts = {},
+    {
+        'nvimdev/lspsaga.nvim',
+        config = function()
+            require('lspsaga').setup({})
+        end,
+        opts = {},
+    },
+
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        opts = {
+            dashboard = {
+                enabled = true,
+                autokeys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                sections = {
+                    { section = "header" },
+                    {
+                        pane = 2,
+                        icon = "",
+                        section = "terminal",
+                        title = "Setup Info",
+                        cmd = "mise current",
+                        indent = 2,
+                        height = 5,
+                        padding = 1,
+                    },
+                    { section = "keys", gap = 1, padding = 1 },
+                    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                    {
+                        pane = 2,
+                        icon = " ",
+                        title = "Git Status",
+                        section = "terminal",
+                        enabled = function()
+                            return Snacks.git.get_root() ~= nil
+                        end,
+                        cmd = "git status --short --branch --renames",
+                        height = 5,
+                        padding = 1,
+                        ttl = 5 * 60,
+                        indent = 3,
+                    },
+                    { section = "startup" },
+                },
+            },
+        },
+    },
 }
