9b3c5acc413826fcea3f9ccc32d6241ab9fdced6
0ta2 <mangamania5643+github@gmail.com>
2025-07-27 02:18:09 +0900

    revert(nvim): bufferline.nvim の不要な設定(config = true)を削除

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -85,7 +85,6 @@ return {
     {
         'akinsho/bufferline.nvim',
         version = "*",
-        config = true,
         dependencies = 'nvim-tree/nvim-web-devicons',
         opts = {
             options = {
