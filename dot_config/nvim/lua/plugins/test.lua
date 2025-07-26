8cf61b58058f65b78b5a8952fdad8abefd17695b
0ta2 <mangamania5643+github@gmail.com>
2025-07-27 04:21:00 +0900

    feat(nvim): neotestを追加

---
+++ dot_config/nvim/lua/plugins/test.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "antoinemadec/FixCursorHold.nvim",
+        { "fredrikaverpil/neotest-golang", version = "*" },
+    },
+    opts = {
+        status = { virtual_text = true },
+        output = { open_on_run = true },
+        adapters = {
+            "neotest-golang",
+        },
+    },
+    config = function(_, opts)
+        local adapters = {}
+        if opts.adapters then
+            for _, config in pairs(opts.adapters or {}) do
+                if type(config) == "string" then
+                    config = require(config)
+                end
+                adapters[#adapters + 1] = config
+            end
+            opts.adapters = adapters
+        end
+
+        require("neotest").setup(opts)
+    end,
+}
