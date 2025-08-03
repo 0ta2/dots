b332abf7eb403ad29431331bcc4bba6e0dde92bb
0ta2 <mangamania5643+github@gmail.com>
2025-08-03 10:50:40 +0900

    feat(nvim): blink.pairs を追加

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -21,5 +21,55 @@ return {
             fuzzy = { implementation = "prefer_rust_with_warning" }
         },
         opts_extend = { "sources.default" }
-    }
+    },
+
+    {
+        'saghen/blink.pairs',
+        version = '*',
+        dependencies = 'saghen/blink.download',
+        opts = {
+            mappings = {
+                enabled = true,
+                disabled_filetypes = {},
+                pairs = {},
+            },
+            highlights = {
+                enabled = true,
+                groups = {
+                    "BlinkPairsRed",
+                    "BlinkPairsBlue",
+                    "BlinkPairsGreen",
+                    "BlinkPairsYellow",
+                    "BlinkPairsPeach",
+                    "BlinkPairsLavender",
+                },
+                matchparen = {
+                    enabled = true,
+                    group = 'MatchParen',
+                },
+            },
+            debug = false,
+        },
+        config = function(_, opts)
+            local palette = require("catppuccin.palettes").get_palette("mocha")
+            local colors = {
+                Red = palette.red,
+                Blue = palette.blue,
+                Green = palette.green,
+                Yellow = palette.yellow,
+                Peach = palette.peach,
+                Lavender = palette.lavender,
+            }
+            for name, color in pairs(colors) do
+                vim.api.nvim_set_hl(0, "BlinkPairs" .. name, { fg = color })
+            end
+            require("blink.pairs").setup(opts)
+        end,
+    },
+
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0",
+        opts = {},
+    },
 }
