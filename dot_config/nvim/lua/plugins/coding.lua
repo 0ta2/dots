3cec7cb27c15a5cee3bdd58c9b0b744d7b3ffba1
0ta2 <mangamania5643+github@gmail.com>
2025-10-14 09:20:05 +0900

    feat(nvim): sidekick.nvimを導入

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -16,9 +16,39 @@ return {
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
-            keymap = { preset = 'super-tab' },
+            keymap = {
+                ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+                ['<C-e>'] = { 'hide', 'fallback' },
 
-            appearance = {
+                ['<Tab>'] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_and_accept()
+                        end
+                    end,
+                    'snippet_forward',
+                    'fallback'
+                },
+                ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+                ['<Up>'] = { 'select_prev', 'fallback' },
+                ['<Down>'] = { 'select_next', 'fallback' },
+                ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+                ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+                ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+                ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+
+                -- signature設定
+                ['<C-u>'] = { 'scroll_signature_up', 'fallback' },
+                ['<C-d>'] = { 'scroll_signature_down', 'fallback' },
+                ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+            },
+
+           appearance = {
                 nerd_font_variant = 'mono'
             },
 
@@ -38,6 +68,12 @@ return {
                         },
                     },
                 },
+                list = {
+                    selection = {
+                        preselect = true,
+                        auto_insert = true,
+                    },
+                },
             },
 
             sources = {
@@ -53,7 +89,11 @@ return {
 
             fuzzy = { implementation = "prefer_rust_with_warning" },
 
-            snippets = { preset = 'luasnip' },
+            snippets = {
+                preset = 'luasnip'
+            },
+
+            signature = { enabled = true }
         },
         opts_extend = { "sources.default" }
     },
