d7e9650289eb7853df1bba3b93351f20055c1367
0ta2 <0ta2@mba.local>
2025-12-26 04:17:30 +0900

    refactor(nvim): デフォルト値を使っているので、定義を削除

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -91,20 +91,7 @@ return {
             },
 
             cmdline = {
-                keymap = {
-                    ["<Tab>"] = { "show_and_insert_or_accept_single", "select_next" },
-                    ["<S-Tab>"] = { "show_and_insert_or_accept_single", "select_prev" },
-
-                    ["<C-space>"] = { "show", "fallback" },
-
-                    ["<C-n>"] = { "select_next", "fallback" },
-                    ["<C-p>"] = { "select_prev", "fallback" },
-                    ["<Right>"] = { "select_next", "fallback" },
-                    ["<Left>"] = { "select_prev", "fallback" },
-
-                    ["<C-y>"] = { "select_and_accept", "fallback" },
-                    ["<C-e>"] = { "cancel", "fallback" },
-                },
+                keymap = { preset = "inherit" },
                 completion = {
                     menu = {
                         auto_show = true,
