8a36e669672e8787f45f6dcec46b00fa710d75b5
0ta2 <0ta2@mba.local>
2026-01-11 05:55:33 +0900

    style(nvim): test.lua をフォーマットの実施

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -1,29 +1,31 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "antoinemadec/FixCursorHold.nvim",
-        { "fredrikaverpil/neotest-golang", version = "*" },
-    },
-    opts = {
-        status = { virtual_text = true },
-        output = { open_on_run = true },
-        adapters = {
-            "neotest-golang",
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "antoinemadec/FixCursorHold.nvim",
+            { "fredrikaverpil/neotest-golang", version = "*" },
         },
-    },
-    config = function(_, opts)
-        local adapters = {}
-        if opts.adapters then
-            for _, config in pairs(opts.adapters or {}) do
-                if type(config) == "string" then
-                    config = require(config)
+        opts = {
+            status = { virtual_text = true },
+            output = { open_on_run = true },
+            adapters = {
+                "neotest-golang",
+            },
+        },
+        config = function(_, opts)
+            local adapters = {}
+            if opts.adapters then
+                for _, config in pairs(opts.adapters or {}) do
+                    if type(config) == "string" then
+                        config = require(config)
+                    end
+                    adapters[#adapters + 1] = config
                 end
-                adapters[#adapters + 1] = config
+                opts.adapters = adapters
             end
-            opts.adapters = adapters
-        end
 
-        require("neotest").setup(opts)
-    end,
+            require("neotest").setup(opts)
+        end,
+    },
 }
