8a666d9f0d99c046eee3ae892f4813fdd5f3f78c
0ta2 <0ta2@mba.local>
2026-01-11 03:41:47 +0900

    feat(nvim): vimのモードを色でわかるようにするプラグインを追加

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -191,4 +191,23 @@ return {
         end,
     },
 
+    {
+        -- vimモードを色で表示する
+        "mvllow/modes.nvim",
+        opts = {
+            colors = {
+                bg = "", -- Optional bg param, defaults to Normal hl group
+                copy = "#f5c359",
+                delete = "#c75c6a",
+                change = "#c75c6a", -- Optional param, defaults to delete
+                format = "#c79585",
+                insert = "#78ccc5",
+                replace = "#245361",
+                select = "#9745be", -- Optional param, defaults to visual
+                visual = "#9745be",
+            },
+
+            line_opacity = 0.4,
+        },
+    },
 }
