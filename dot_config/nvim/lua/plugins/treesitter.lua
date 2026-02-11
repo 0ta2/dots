a9db8bcb8613e018906f6e8fbc49c9b29b156545
0ta2 <0ta2@mba.local>
2026-02-11 22:06:33 +0900

    feat(nvim): treesitterをmainブランチに変更したため、mainブランチの設定に追従

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,43 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"lua",
-				"go",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			-- 100 KBを超える場合は､自動的に無効
-			disable = function(lang, buf)
-				local max_filesize = 100 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        config = function(_, opts)
+            local treesitter = require("nvim-treesitter")
+            local installed = treesitter.get_installed()
+            local to_install = vim.tbl_filter(function(lang)
+                return not vim.tbl_contains(installed, lang)
+            end, opts.langs)
+            if #to_install > 0 then
+                treesitter.install(to_install)
+            end
 
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		opts = {},
-	},
+            local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+            vim.api.nvim_create_autocmd("FileType", {
+                group = group,
+                pattern = opts.langs,
+                callback = function(args)
+                    vim.treesitter.start(args.buf)
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
+        opts = {
+            langs = {
+                "zsh",
+                "bash",
+                "lua",
+                "go",
+                "rust",
+                "python",
+                "terraform",
+                "sql"
+            },
+        },
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = {},
+    },
 }
